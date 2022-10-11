ed8c2d823cd8903f5af7fef08cee2cf0ec4c36a2
Sintija Linuza <sintija.linuza@gmail.com>
2022-10-11 18:26:29 +0100

    added a placeholder jump

--- app.js
+++ app.js
@@ -30,6 +30,19 @@ containers.forEach ((container) => {
                 {attr: {d: end}, ease: 'Power2.easeOut', duration: 0.75}
             );
             tl.to(line, {attr: {d: start}, ease: 'elastic.out(3, 0.5'}, '<50%')
+
+            //placeholder shift 
+            tl.to(placeholder,{
+                top: -15, 
+                left: 0,
+                scale: 0.7,
+                 duration: 0.5,
+                  ease: 'Power2.easeOut' 
+                },
+                "<15%"
+                
+                )
+
         }
     });
 });
